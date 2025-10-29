b34c1d93a743d7220cb49719838d81903839a456
Evan Boodoosingh <evan9496@gmail.com>
2025-10-29 16:00:48 -0400

    fix: correct typo in Logo component and adjust Navbar layout

--- src/Ui/Navbar.jsx
+++ src/Ui/Navbar.jsx
@@ -5,7 +5,17 @@ function Navbar() {
     <nav className="fixed top-0 w-full z-40 bg[hsla(0, 0%, 4%, 0.80)] backdrop-blur-lg px-1 border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16 font-mono">
-            <Logo />
+          <Logo />
+          <div className="flex md:flex items-center space-x-8">
+            <div className="hidden md:flex items-center space-x-8">
+              <a
+                href="#home"
+                className="text-gray-300 hover:text-xl hover:text-white transition-all"
+              >
+                Home
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
