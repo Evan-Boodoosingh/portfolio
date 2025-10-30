5b574a070557ba6e68c23a18da25e7ae2e2ac90e
Evan Boodoosingh <evan9496@gmail.com>
2025-10-29 23:10:49 -0400

    feat: enhance Navbar component to manage menu state and add menu icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Ui/NavBar";
 
 function App() {
-  return <div className="text-3xl font-bold underline">
-
-    <Navbar />
-  </div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="text-3xl font-bold underline">
+      <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+    </div>
+  );
 }
 
 export default App;
