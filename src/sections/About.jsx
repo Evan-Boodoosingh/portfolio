afcd788562fcce12e4a622bb54b144e69fd1c55c
Evan Boodoosingh <evan9496@gmail.com>
2026-01-12 10:40:13 -0500

    feat: wrap About section content in RevealOnScroll component for enhanced visibility

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -14,10 +14,11 @@ function About() {
   ];
   return (
     <section id="about" className="py-10 pt-32 font-mono">
-      <div className="max-w-4xl mx-auto px-4">
-        <h2 className="max-w-4xl text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold mb-8 bg-linear-to-r from-teal-400 via-blue-500 to-indigo-600 bg-clip-text text-transparent text-center leading-tight">
-          About Me
-        </h2>
+      <RevealOnScroll>
+        <div className="max-w-4xl mx-auto px-4">
+          <h2 className="max-w-4xl text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold mb-8 bg-linear-to-r from-teal-400 via-blue-500 to-indigo-600 bg-clip-text text-transparent text-center leading-tight">
+            About Me
+          </h2>
         <div className="mb-8 p-6 rounded-3xl border-white/10 border-2">
           <p className="text-sm sm:text-base leading-snug indent-6 text-gray-300 mb-4">
             I am a Full-Stack MERN Developer whose passion is built into every
